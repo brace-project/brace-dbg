eece8b9a50cf5fe6a2d79035a289f7a110847460
Matthes <matthes@leuffen.de>
2021-10-29 17:50:09 +0200

    update

--- src/BraceDbg.php
+++ src/BraceDbg.php
@@ -25,22 +25,79 @@ class BraceDbg
     }
 
 
+    private static function _setupErrorHandler(bool $verbose = false)
+    {
+        set_error_handler(function ($errNo, $errStr, $errFile, $errLine) {
+            $msg = "$errStr in $errFile on line $errLine";
+            if ($errNo == E_NOTICE || $errNo == E_WARNING) {
+                throw new \ErrorException($msg, $errNo);
+            } else {
+            }
+        });
+
+        set_exception_handler(function (\Exception|\Error $ex) use ($verbose) {
+            header("Content-Type: text/plain");
+            header("HTTP/1.1 500 Internal Server Error");
+            echo "HTTP/1.1 500 Internal Server Error";
+
+            if ($verbose === true) {
+                $previous = $ex;
+                echo "\n\nUncaught " . get_class($ex) . ": " . $ex->getMessage();
+
+                while ($previous !== null) {
+                    echo "\n\n---";
+                    echo "\n" . get_class($previous) . " Msg: '{$previous->getMessage()}' Code: {$previous->getCode()}\n";
+
+                    echo "\nin file: " . $previous->getFile() . "(". $previous->getLine().")";
+                    echo "\n\n";
+                    echo $previous->getTraceAsString();
+                    echo "\n===";
+                    $previous = $previous->getPrevious();
+                }
+            }
+            throw $ex;
+        });
+    }
+
+    private static function isDevModeAutodetect(array $allowHosts) : bool
+    {
+        if ( ! isset ($_SERVER["HTTP_HOST"]))
+            return false;
+        return in_array($_SERVER["HTTP_HOST"], $allowHosts);
+    }
+
+    private static function registerDefaultLogger(string $log_file="php://stderr")
+    {
+        self::$logger = function (...$params) {
+            $ret=[];
+            foreach ($params as $param) {
+                $ret[] = self::GetType($param);
+            }
+            file_put_contents("php://stderr", "out(" . implode(", ", $ret) . ");\n");
+        };
+    }
+
+
+    public static bool $developmentMode = false;
+
     public static \Closure|bool $logger = false;
 
     public static function SetupEnvironment(
+        bool $autodetect_developement_mode = true,
+        array $development_mode_hosts = ["localhost"],
         callable $logger = null,
+        string $log_file = "php://stderr"
     ) {
-        if ($logger === null) {
-            $logger = fn(...$out) => file_put_contents("php://stderr", "out(" . implode(", ", array_filter($out, fn ($in) => "'" . print_r($in, true) . "'")) . ")\n");
-            $logger = function (...$params) {
-                $ret=[];
-                foreach ($params as $param) {
-                    $ret[] = self::GetType($param);
-                }
-                file_put_contents("php://stderr", "out(" . implode(", ", $ret) . ");\n");
-            };
+
+        if ($autodetect_developement_mode) {
+            self::$developmentMode = self::isDevModeAutodetect($development_mode_hosts);
         }
-        self::$logger = $logger;
+
+        if ($logger === null && self::$developmentMode) {
+            self::registerDefaultLogger($log_file);
+        }
+        out(self::$developmentMode);
+        self::_setupErrorHandler(self::$developmentMode);
     }
 
 }
--- src/functions.php
+++ src/functions.php
@@ -4,10 +4,10 @@
 function out(...$data) : void
 {
     static $logger = null;
-    if ($logger === false)
-        return;
     if ($logger === null)
         $logger = \Brace\Dbg\BraceDbg::$logger;
+    if ($logger === false)
+        return;
     $logger(...$data);
 }
 
