78d4fa002bdf0795e0f93a08d0b3ab08fffed558
Matthes <matthes@leuffen.de>
2021-10-29 16:13:54 +0200

    update

--- src/BraceDbg.php
+++ src/BraceDbg.php
@@ -6,6 +6,25 @@ class BraceDbg
 {
 
 
+    public static function GetType($input) {
+        if (is_string($input)) return "'" . $input ."'";
+        if ($input === true) return "true";
+        if ($input === false) return "false";
+        if ($input === null) return "NULL";
+        if (is_int($input)) return (string)$input;
+        if (is_float($input)) return (string)$input;
+        if (is_array($input) || is_object($input)) {
+            $ret = print_r ($input, true);
+            if (strlen($ret) < 255) {
+                return preg_replace("/\n[ ]*/m", " ", $ret);
+            } else {
+                return "\n" . print_r ($input, true). "";
+            }
+        }
+        return get_debug_type($input);
+    }
+
+
     public static \Closure|bool $logger = false;
 
     public static function SetupEnvironment(
@@ -13,6 +32,13 @@ class BraceDbg
     ) {
         if ($logger === null) {
             $logger = fn(...$out) => file_put_contents("php://stderr", "out(" . implode(", ", array_filter($out, fn ($in) => "'" . print_r($in, true) . "'")) . ")\n");
+            $logger = function (...$params) {
+                $ret=[];
+                foreach ($params as $param) {
+                    $ret[] = self::GetType($param);
+                }
+                file_put_contents("php://stderr", "out(" . implode(", ", $ret) . ");\n");
+            };
         }
         self::$logger = $logger;
     }
